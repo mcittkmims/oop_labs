137e94eec4f94143f02d5448dae4ea0393be0b1f
mcittkmims <adrian.vremere.swe@gmail.com>
2024-10-13 00:47:02 +0300

    Day 1: Initialized my workspace and designed the class around the main data of the laboratory. Checked if everything works correclty.

---
+++ lab_0-papers-please/java-classifcation/src/main/java/Alien.java
@@ -0,0 +1,72 @@
+import java.util.List;
+
+public class Alien {
+    private int id;
+    private boolean isHumanoid;
+    private String planet;
+    private int age;
+    private List<String> traits;
+
+    // Constructor
+    public Alien(int id, boolean isHumanoid, String planet, int age, List<String> traits) {
+        this.id = id;
+        this.isHumanoid = isHumanoid;
+        this.planet = planet;
+        this.age = age;
+        this.traits = traits;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isHumanoid() {
+        return isHumanoid;
+    }
+
+    public void setHumanoid(boolean humanoid) {
+        isHumanoid = humanoid;
+    }
+
+    public String getPlanet() {
+        return planet;
+    }
+
+    public void setPlanet(String planet) {
+        this.planet = planet;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getTraits() {
+        return traits;
+    }
+
+    public void setTraits(List<String> traits) {
+        this.traits = traits;
+    }
+
+    @Override
+    public String toString() {
+        return "Alien{" +
+                "id=" + id +
+                ", isHumanoid=" + isHumanoid +
+                ", planet='" + planet + '\'' +
+                ", age=" + age +
+                ", traits=" + traits +
+                '}';
+    }
+}
+
+
