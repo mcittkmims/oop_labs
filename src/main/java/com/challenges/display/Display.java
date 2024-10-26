87237e5667d4bbeefa2e51fff1e23d5d5bda8faa
mcittkmims <adrian.vremere.swe@gmail.com>
2024-10-26 18:04:12 +0300

    Created the class skeleton based of UML

--- src/main/java/com/challenges/display/Display.java
+++ src/main/java/com/challenges/display/Display.java
@@ -1,5 +1,21 @@
 package com.challenges.display;
 
 public class Display {
+    private int width;
+    private int height;
+    private float ppi;
+    private String model;
+
+    public void compareSize(Display m) {
+
+    }
+
+    public void compareSharpness(Display m) {
+
+    }
+
+    public void compareWithMonitor(Display m) {
+
+    }
     
 }
