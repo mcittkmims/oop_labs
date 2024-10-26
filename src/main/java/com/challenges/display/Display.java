98196b4ae6efadfe51816d1a8aac418f82198398
mcittkmims <adrian.vremere.swe@gmail.com>
2024-10-26 19:45:42 +0300

    Implemented the method compareSize from Display class

--- src/main/java/com/challenges/display/Display.java
+++ src/main/java/com/challenges/display/Display.java
@@ -6,8 +6,46 @@ public class Display {
     private float ppi;
     private String model;
 
-    public void compareSize(Display m) {
+    public Display(int width, int height, float ppi, String model) {
+        this.width = width;
+        this.height = height;
+        this.ppi = ppi;
+        this.model = model;
+    }
 
+    public void compareSize(Display m) {
+        // If both width and height are equal, the models have the same size
+        if (this.width == m.width && this.height == m.height) {
+            System.out.println(this.model + " has the same size as " + m.model);
+            return;
+        }
+        // If both width and height are bigger, the model is bigger
+        if (this.width > m.width && this.height > m.height) {
+            System.out.println(this.model + " is bigger than " + m.model);
+            return;
+        }
+        // If both width and height are smaller, the model is smaller
+        if (this.width < m.width && this.height < m.height) {
+            System.out.println(this.model + " is smaller than " + m.model);
+            return;
+        }
+
+        // Checks each attribute individually
+        if (this.width > m.width) {
+            System.out.print(this.model + " is longer and ");
+        } else if (this.width > m.width) {
+            System.out.print(this.model + " is narrower and ");
+        } else {
+            System.out.print(this.model + " is the same width and ");
+        }
+
+        if (this.height > m.height) {
+            System.out.print(" taller than " + m.model);
+        } else if (this.height > m.height) {
+            System.out.print(" the same height as " + m.model);
+        } else {
+            System.out.print(" shorter than " + m.model);
+        }
     }
 
     public void compareSharpness(Display m) {
@@ -17,5 +55,5 @@ public class Display {
     public void compareWithMonitor(Display m) {
 
     }
-    
+
 }
