c7f72b22b69c056dcf35ce9062e531ee5034cdd1
mcittkmims <adrian.vremere.swe@gmail.com>
2024-10-27 02:35:59 +0300

    Day 3: Implemented buyDisplay method from Assistant class

--- src/main/java/com/challenges/assistant/Assistant.java
+++ src/main/java/com/challenges/assistant/Assistant.java
@@ -14,10 +14,12 @@ public class Assistant {
         this.assignedDisplays = new ArrayList<>();
     }
 
+    // Adding a display to the list
     public void assignDisplay(Display d) {
         this.assignedDisplays.add(d);
     }
     
+    // Method to compare each display with the next display in the list
     public void assist() {
         for (int i = 0; i < this.assignedDisplays.size() - 1; i++) {
 
@@ -29,5 +31,27 @@ public class Assistant {
         }
     }
 
+    // Method to remove a specific Display object
+    public Display buyDisplay(Display d) {
+        Display boughtDisplay = null;
 
+        // It works even if the Display d has different reference
+        for (Display display : this.assignedDisplays) {
+            if (display.equals(d)) {
+                System.out.println(display);
+                boughtDisplay = display;
+                this.assignedDisplays.remove(display);
+                break;
+            }
+        }
+        return boughtDisplay;
+    }
+
+    public String getAssistantName() {
+        return this.assistantName;
+    }
+
+    public List<Display> assignedDisplays() {
+        return this.assignedDisplays;
+    }
 }
