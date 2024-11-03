fd2377614f9ed023b4b80d5269a215f663c7e922
mcittkmims <adrian.vremere.swe@gmail.com>
2024-11-04 00:07:15 +0200

    Task 4: Implemented the main method to ask for questions

--- src/main/java/com/coffee/barista/Main.java
+++ src/main/java/com/coffee/barista/Main.java
@@ -1,16 +1,132 @@
 package com.coffee.barista;
 
+import java.util.Scanner;
+
 import com.coffee.barista.cafe.*;
 import com.coffee.enums.Intensity;
 import com.coffee.enums.SyrupType;
 
 public class Main {
-     public static void main(String[] args) {
-        SyrupCappuccino syrupCappuccino = new SyrupCappuccino(Intensity.NORMAL, 50, SyrupType.CARAMEL).makeSyrupCappuccino();
-        Cappuccino cappuccino = new Cappuccino(Intensity.LIGHT, 20).makeCappuccino();
-        Americano americano = new Americano(Intensity.STRONG, 100).makeAmericano();
-        syrupCappuccino.printDetails();
-        cappuccino.printDetails();
-        americano.printDetails();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Coffee in menu:\n" +
+                "1. Cappuccino\n" +
+                "2. Americano\n" +
+                "3. PumpkinSpiceLatte\n" +
+                "4. SyrupCappuccino");
+
+        String orders = "";
+        System.out.print("What coffee would you like? ");
+        while (true) {
+            String coffee = scanner.nextLine();
+            if (isValidCoffee(coffee.toUpperCase())) {
+                orders += coffee.toUpperCase() + ",";
+
+                System.out.print("What intensity do you want? (LIGHT, NORMAL, STRONG): ");
+                while (true) {
+                    String intensity = scanner.nextLine();
+                    if (isValidIntensity(intensity.toUpperCase())) {
+                        orders += intensity.toUpperCase() + ",";
+                        break;
+                    }
+                    System.out.print("Try again: ");
+                }
+
+                System.out.print("How many ml of milk/water do you want?: ");
+                while (true) {
+                    String number = scanner.nextLine();
+                    if (isValidNumber(number)) {
+                        orders += number + ",";
+                        break;
+                    }
+                    System.out.print("Not a valid cantity! ");
+                }
+
+                if (coffee.toUpperCase().equals("PUMPKINSPICELATTE")) {
+                    System.out.print("How many mg of pumpkin spice do you want?: ");
+                    while (true) {
+                        String number = scanner.nextLine();
+                        if (isValidNumber(number)) {
+                            orders += number + ",";
+                            break;
+                        }
+                        System.out.print("Not a valid cantity! ");
+                    }
+                }
+                if (coffee.toUpperCase().equals("SYRUPCAPPUCCINO")) {
+                    System.out.println(
+                            "What type of syrup do you want? (MACADAMIA, VANILLA, COCONUT, CARAMEL, CHOCOLATE, POPCORN): ");
+                    while (true) {
+                        String syrup = scanner.nextLine();
+                        if (isValidSyrup(syrup.toUpperCase())) {
+                            orders += syrup.toUpperCase() + ",";
+                            break;
+                        }
+                        System.out.print("Not a valid syrup! ");
+                    }
+                }
+                System.out.println("Would you like another coffee?(yes or no)");
+                boolean option;
+                while (true) {
+                    String choice = scanner.nextLine();
+                    if (choice.equals("no")) {
+                        option = true;
+                        break;
+                    } else if (choice.equals("yes")) {
+                        option = false;
+                        break;
+                    }
+                    System.out.print("Not a valid choice! ");
+                }
+                if (option) {
+                    break;
+                }
+                System.out.print("What other coffee would you like? ");
+
+            } else {
+                System.out.print("Not a valid coffee type! ");
+            }
+        }
+
+        System.out.println(orders);
+
+    }
+
+    public static boolean isValidIntensity(String intensity) {
+        try {
+            Enum.valueOf(Intensity.class, intensity);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public static boolean isValidNumber(String number) {
+        try {
+            Integer.valueOf(number);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        if (Integer.valueOf(number) > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValidCoffee(String coffee) {
+        return coffee.equals("CAPPUCCINO")
+                || coffee.equals("AMERICANO")
+                || coffee.equals("PUMPKINSPICELATTE")
+                || coffee.equals("SYRUPCAPPUCCINO");
+    }
+    
+    public static boolean isValidSyrup(String syrup) {
+        try {
+            Enum.valueOf(SyrupType.class, syrup);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
